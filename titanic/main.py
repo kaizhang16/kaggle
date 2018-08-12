5a97f735edd8a8911a35ad64cef5bbf30f645a46
Kai Zhang <kaizhang91@foxmail.com>
2018-08-12 13:07:59 +0800

    sort objects

--- titanic/main.py
+++ titanic/main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+import matplotlib.pyplot as plt
+import numpy as np
+import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import xgboost as xgb
 
 
@@ -12,24 +16,25 @@ class Data:
 
 
 def object_to_int(object_, object_list):
-    for i, x in enumerate(object_list):
+    xs = sorted([x for x in object_list if x is not np.nan])
+    for i, x in enumerate(xs):
         if x == object_:
             return i
 
-    return -1
+    return np.nan
 
 
-def object_column_to_int(data, column_name):
+def column_object_to_int(data, column_name):
     data[column_name] = data.apply(
         lambda row: object_to_int(row[column_name], data[column_name].unique()),
         axis=1)
 
 
 def get_X_y(raw_data):
-    raw_data = raw_data.drop(['Name', 'Ticket'], axis=1)
-    object_column_to_int(raw_data, 'Sex')
-    object_column_to_int(raw_data, 'Cabin')
-    object_column_to_int(raw_data, 'Embarked')
+    raw_data = raw_data.drop(
+        ['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1)
+    column_object_to_int(raw_data, 'Sex')
+    column_object_to_int(raw_data, 'Embarked')
     X = raw_data.drop(['Survived'], axis=1)
     y = raw_data.Survived
     return X, y
@@ -47,20 +52,19 @@ def train(data_train):
     return xgb.XGBClassifier().fit(data_train.X, data_train.y)
 
 
+def realpath(relative_path):
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(current_dir, relative_path)
+
+
 def main():
-    data_train, data_test = load_train_test('data/train.csv')
-    # print('>>> data_train.X:\n{}'.format(data_train.X))
-    # print('>>> data_train.y:\n{}'.format(data_train.y))
-    # print('>>> data_test.X:\n{}'.format(data_test.X))
+    data_train, data_test = load_train_test(realpath('data/train.csv'))
     model = train(data_train)
     predictions = model.predict(data_test.X)
-    # print('>>> predictions: {}'.format(predictions))
-    right = 0
-    for i, p in enumerate(predictions):
-        if p == data_test.y.iloc[i]:
-            right += 1
-    accuracy = right / len(data_test.y)
+    accuracy = accuracy_score(data_test.y, predictions)
     print('>>> accuracy: {}'.format(accuracy))
+    xgb.plot_tree(model)
+    plt.show()
 
 
 if __name__ == "__main__":
